e7ffb1f3574f35b6a6ac728b4231be8bba465d77
Lewey Metcalf <leweymetcalf@hotmail.co.uk>
2020-11-17 20:22:02 +0100

    refactor tests. Add tuple summing

--- tuple.go
+++ tuple.go
@@ -19,3 +19,12 @@ func (t Tuple) isVector() bool {
 func (t Tuple) isPoint() bool {
 	return t.w == 1.0
 }
+
+func (t Tuple) add(t2 *Tuple) *Tuple {
+	return &Tuple{
+		x: t.x + t2.x,
+		y: t.y + t2.y,
+		z: t.z + t2.z,
+		w: t.w + t2.w,
+	}
+}
--- tuple_test.go
+++ tuple_test.go
@@ -5,100 +5,80 @@ import (
 	"github.com/cucumber/godog"
 )
 
-var a *Tuple
+var tuples map[string]*Tuple
 
-func aTuple(arg1, arg2, arg3, arg4 float64) error {
-	a = &Tuple{arg1, arg2, arg3, arg4}
+func setTuple(tuple string, arg1, arg2, arg3, arg4 float64) error {
+	tuples[tuple] = &Tuple{arg1, arg2, arg3, arg4}
 	return nil
 }
 
-func ax(arg1 float64) error {
-	if a.x != arg1 {
-		return fmt.Errorf("x should be %f", arg1)
-	}
+func setPoint(p string, arg1, arg2, arg3 float64) error {
+	tuples[p] = NewPoint(arg1, arg2, arg3)
 	return nil
 }
 
-func ay(arg1 float64) error {
-	if a.y != arg1 {
-		return fmt.Errorf("y should be %g", arg1)
-	}
+func setVector(v string, arg1, arg2, arg3 float64) error {
+	tuples[v] = NewVector(arg1, arg2, arg3)
 	return nil
 }
 
-func az(arg1 float64) error {
-	if a.z != arg1 {
-		return fmt.Errorf("z should be %g", arg1)
-	}
-	return nil
+func IsAPoint(t string) error {
+	return expectTrue(tuples[t].isPoint(), fmt.Sprintf("%v should be a point", *tuples["a"]))
 }
 
-func aw(arg1 float64) error {
-	if a.w != arg1 {
-		return fmt.Errorf("w should be %g", arg1)
-	}
-	return nil
+func IsNotAVector(t string) error {
+	return expectFalse(tuples[t].isVector(), "should not be a vector")
 }
 
-func aIsAPoint() error {
-	if !a.isPoint() {
-		return fmt.Errorf("%v should be a point", *a)
-	}
-	return nil
+func IsNotAPoint(t string) error {
+	return expectFalse(tuples[t].isPoint(), fmt.Sprintf("%v should be a point", *tuples["a"]))
 }
 
-func aIsNotAVector() error {
-	if a.isVector() {
-		return fmt.Errorf("should not be a vector")
-	}
-	return nil
+func IsAVector(t string) error {
+	return expectTrue(tuples[t].isVector(), "should be a vector")
 }
 
-func aIsNotAPoint() error {
-	if a.isPoint() {
-		return fmt.Errorf("%v should not be a point", *a)
-	}
-	return nil
+func addTuples(t1, t2 string, arg1, arg2, arg3, arg4 float64) error {
+	tuple1 := tuples[t1]
+	tuple2 := tuples[t2]
+	tupleSum := tuple1.add(tuple2)
+	return expectTuple(tupleSum, arg1, arg2, arg3, arg4)
 }
 
-func aIsAVector() error {
-	if !a.isVector() {
-		return fmt.Errorf("should be a vector")
-	}
-	return nil
-}
-
-func aPoint(arg1, arg2, arg3 float64) error {
-	a = NewPoint(arg1, arg2, arg3)
-	return nil
-}
-
-func aVector(arg1, arg2, arg3 float64) error {
-	a = NewVector(arg1, arg2, arg3)
-	return nil
-}
-
-func aEqualsTuple(arg1, arg2, arg3, arg4 float64) error {
-	if a.x == arg1 && a.y == arg2 && a.z == arg3 && a.w == arg4 {
-		return nil
-	}
-	return fmt.Errorf("%v should have values (%g, %g, %g, %g)", *a, arg1, arg2, arg3, arg4)
+func equalsTuple(t string, arg1, arg2, arg3, arg4 float64) error {
+	tuple := tuples[t]
+	return expectTuple(tuple, arg1, arg2, arg3, arg4)
 }
 
 func InitializeScenario(s *godog.ScenarioContext) {
-	s.Step(`^a ← tuple\((\-*\d+\.\d+), (\-*\d+\.\d+), (\-*\d+\.\d+), (\-*\d+\.\d+)\)$`, aTuple)
-	s.Step(`^a ← point\((\-*\d+\.\d+), (\-*\d+\.\d+), (\-*\d+\.\d+)\)$`, aPoint)
-	s.Step(`^a ← vector\((\-*\d+\.\d+), (\-*\d+\.\d+), (\-*\d+\.\d+)\)$`, aVector)
-	s.Step(`^a\.x = (\-*\d+\.\d+)$`, ax)
-	s.Step(`^a\.y = (\-*\d+\.\d+)$`, ay)
-	s.Step(`^a\.z = (\-*\d+\.\d+)$`, az)
-	s.Step(`^a\.w = (\-*\d+\.\d+)$`, aw)
-	s.Step(`^a is a point$`, aIsAPoint)
-	s.Step(`^a is not a vector$`, aIsNotAVector)
-	s.Step(`^a is a vector$`, aIsAVector)
-	s.Step(`^a is not a point$`, aIsNotAPoint)
-	s.Step(`^a = tuple\((\-*\d+\.\d+), (\-*\d+\.\d+), (\-*\d+\.\d+), (\-*\d+\.\d+)\)$`, aEqualsTuple)
+	s.Step(`^([A-Za-z0-9]*) ← tuple\((\-*\d+\.\d+), (\-*\d+\.\d+), (\-*\d+\.\d+), (\-*\d+\.\d+)\)$`, setTuple)
+	s.Step(`^([A-Za-z0-9]*) ← point\((\-*\d+\.\d+), (\-*\d+\.\d+), (\-*\d+\.\d+)\)$`, setPoint)
+	s.Step(`^([A-Za-z0-9]*) ← vector\((\-*\d+\.\d+), (\-*\d+\.\d+), (\-*\d+\.\d+)\)$`, setVector)
+	s.Step(`^([A-Za-z0-9]*) is a point$`, IsAPoint)
+	s.Step(`^([A-Za-z0-9]*) is not a vector$`, IsNotAVector)
+	s.Step(`^([A-Za-z0-9]*) is a vector$`, IsAVector)
+	s.Step(`^([A-Za-z0-9]*) is not a point$`, IsNotAPoint)
+	s.Step(`^([A-Za-z0-9]*) = tuple\((\-*\d+\.\d+), (\-*\d+\.\d+), (\-*\d+\.\d+), (\-*\d+\.\d+)\)$`, equalsTuple)
+	s.Step(`^([A-Za-z0-9]*) \+ ([A-Za-z0-9]*) = tuple\((\-*\d+\.\d+), (\-*\d+\.\d+), (\-*\d+\.\d+), (\-*\d+\.\d+)\)$`, addTuples)
 	s.BeforeScenario(func(sc *godog.Scenario) {
-		a = nil
+		tuples = make(map[string]*Tuple)
 	})
+}
+
+func expectTuple(tuple *Tuple, arg1 float64, arg2 float64, arg3 float64, arg4 float64) error {
+	if tuple.x == arg1 && tuple.y == arg2 && tuple.z == arg3 && tuple.w == arg4 {
+		return nil
+	}
+	return fmt.Errorf("%v should have values (%g, %g, %g, %g)", *tuple, arg1, arg2, arg3, arg4)
+}
+
+func expectTrue(cond bool, errMsg string) error {
+	if !cond {
+		return fmt.Errorf(errMsg)
+	}
+	return nil
+}
+
+func expectFalse(cond bool, errMsg string) error {
+	return expectTrue(!cond, errMsg)
 }
